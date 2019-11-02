d329a3f4848ccd59d788d1e970f944e625e75cbe
Mårten Åsberg <marten.asberg@outlook.com>
2019-11-02 23:06:13 +0100

    The player can now take input from other programs

--- Player/Program.cs
+++ Player/Program.cs
@@ -1,13 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace MoreTec.Player
 {
-	class Program
+	static class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
-			Player player = new Player(3, 3, Console.In, Console.Out);
+			Dictionary<string, string> argsMap = ParseArguments(args);
+
+
+			TextReader crossInput;
+			TextWriter crossOutput;
+			if (argsMap.ContainsKey("cross"))
+			{
+				Process cross = Process.Start(argsMap["cross"]);
+				crossInput = cross.StandardOutput;
+				crossOutput = cross.StandardInput;
+			}
+			else
+			{
+				crossInput = Console.In;
+				crossOutput = Console.Out;
+			}
+
+			TextReader circleInput;
+			TextWriter circleOutput;
+			if (argsMap.ContainsKey("circle"))
+			{
+				Process circle = Process.Start(argsMap["circle"]);
+				circleInput = circle.StandardOutput;
+				circleOutput = circle.StandardInput;
+			}
+			else
+			{
+				circleInput = Console.In;
+				circleOutput = Console.Out;
+			}
+
+			Player player = new Player(3, 3, crossInput, crossOutput, circleInput, circleOutput);
 			player.Play();
+		}
+
+		private static Dictionary<string, string> ParseArguments(string[] args)
+		{
+			Dictionary<string, string> argsMap = new Dictionary<string, string>();
+
+			for (int i = 0; i < args.Length; i++)
+			{
+				if (args[i].StartsWith("--"))
+				{
+					string key = args[i].Substring(2).ToLower();
+					string value = null;
+					if (i + 1 < args.Length && !args[i + 1].StartsWith("--"))
+					{
+						value = args[i + 1];
+						i++;
+					}
+
+					argsMap.Add(key, value);
+				}
+			}
+
+			return argsMap;
 		}
 	}
 }
