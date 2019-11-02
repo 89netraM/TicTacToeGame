e6675e137fde8f5152c861c564005ef7c87c919b
Mårten Åsberg <marten.asberg@outlook.com>
2019-11-02 20:51:26 +0100

    String representations of the different game objects

---
+++ Player/StringRepresentations.cs
@@ -0,0 +1,38 @@
+﻿using MoreTec.TicTacToe;
+using System;
+using System.Text;
+
+namespace MoreTec.Player
+{
+	static class StringRepresentations
+	{
+		public static string Board(Board board)
+		{
+			string[] rows = new string[board.Size];
+
+			for (int x = 0; x < board.Size; x++)
+			{
+				StringBuilder rowBuilder = new StringBuilder();
+
+				for (int y = 0; y < board.Size; y++)
+				{
+					rowBuilder.Append(Marker(board[x, y]));
+				}
+
+				rows[x] = rowBuilder.ToString();
+			}
+
+			return String.Join('\n', rows);
+		}
+
+		public static string Marker(Marker? marker)
+		{
+			return marker switch
+			{
+				TicTacToe.Marker.Cross => "X",
+				TicTacToe.Marker.Circle => "O",
+				_ => " "
+			};
+		}
+	}
+}
